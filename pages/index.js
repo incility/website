9a590b31c57f265c9c3f251466766447369e58fc
Valentin Madrid <valentin@incility.com>
2022-05-02 18:45:30 +0200

    🏜️ - Change Text on Landing Page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <image className={styles.logo} src="/public/images/logo.png" alt="logo" />
-      <p className={styles.description}>probably just another boring blockchain company</p>
+      <image className={styles.logo} src='/public/images/logo.png' alt='logo' />
+      <p className={styles.description}>
+        cool company focused on blockchain innovation and education.
+        <br />
+        backed by <i className={styles.skillash}>skillhash</i>
+      </p>
     </div>
-  )
+  );
 }
